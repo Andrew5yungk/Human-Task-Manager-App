35f415bfb30aa99e8178acef690c6e5ebd2eeb2a
Andrew Yungk <andrew5yungk@gmail.com>
2025-12-18 13:49:41 -0600

    Commit 5

--- HumanTaskManager/src/Home.jsx
+++ HumanTaskManager/src/Home.jsx
@@ -28,12 +28,13 @@ function Home() {
         .then(() => setTask(task => task.filter(t => t._id !== id)))
         .catch(err => console.log(err))
     }
-
+// toggles the completion of a task
     const handleToggle = (id) => {
         axios.put(`http://localhost:3001/update/${id}`)
         .then(() => setTask(task => task.map(t => t._id === id ? {...t, done: !t.done} : t)))
         .catch(err => console.log(err))
     }
+// will fix the checkbox bug //
 
   return (
     <div className="App">
@@ -45,9 +46,9 @@ function Home() {
             <div><h2>No Tasks ATM</h2></div>
             :
             task.map(task => (
-                <div className="task" style={{border: task.done ? '1px solid red' : '', boxShadow: task.done ? '0 4px 8px 0 red, 0 6px 20px 0 red' : '', borderRadius: task.done ? '5px' : '', taskHover: task.done ? 'box-shadow: 0 8px 16px 0 red, 0 12px 40px 0 red' : ''}}>
+                <div className="task" style={{border: task.done ? '1px solid red' : '', boxShadow: task.done ? '0 4px 8px 0 red, 0 6px 20px 0 red' : '', borderRadius: task.done ? '5px' : '', taskHover: task.done ? 'box-shadow: 0 8px 16px 0 red, 0 12px 40px 0 red' : ''}}> 
                     <div>
-                        <input type="checkbox" id="checkbox" checked={task.done} onChange={() => handleToggle(task._id)}/>
+                        <input type="checkbox" id="checkbox" checked={task.done} onChange={() => handleToggle(task._id)}/> 
                         <span className={task.done ? "strikethrough" : ""}>{task.task}</span>
                         <span className="material-delete" onClick={() => handleDelete(task._id)}><MdDelete /></span> 
                     </div>
